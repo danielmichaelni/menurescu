a5d3813ff6febb565a5b2bb8a0655d1475f81b7e
danielmichaelni <danielmichaelni@gmail.com>
2020-03-21 15:38:59 -0400

    add clear button to inputs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,6 +56,7 @@ const IndexPage = () => {
         </div>
         <div className="searchContainer">
           <Input
+            allowClear
             value={searchValue}
             onChange={handleChange}
             placeholder="Search for a spot..."
@@ -65,6 +66,7 @@ const IndexPage = () => {
         </div>
         <div className="neighborhoodContainer">
           <Select
+            allowClear
             mode="multiple"
             onChange={(value: string[]) => {
               setNeighborhoodFilter(value);
